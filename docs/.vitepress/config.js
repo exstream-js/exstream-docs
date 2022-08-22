74d7d4aff838a29ab661ae33b008eec8c95273c0
Michele Triaca <michele.triaca@telnext.com>
2022-08-22 15:28:00 +0200

    new chapters

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,198 +15,199 @@ export default defineConfig({
       // algoliaOptions: { facetFilters: ['tags:guide,api'] }
     },    
     nav: [
-      { text: 'Guide', link: '/guide/what-is-exstream', activeMatch: '/guide/' }      
+      { text: 'Guide', link: '/guide/what-is-exstream', activeMatch: '/guide/' },
+      { text: 'Reference', link: '/reference/filter', activeMatch: '/reference/' }
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/micheletriaca/exstream' },
     ],  
-    sidebar: [
-      {
-        text: 'Introduction',
-        collapsible: true,
-        items: [
-          { text: 'What is exstream.js?', link: '/guide/what-is-exstream' },
-          { text: 'Getting Started', link: '/guide/getting-started' }
-        ]
-      },
-      {
-        text: 'Guide',
-        collapsible: true,
-        items: [
-          { text: 'Basic concepts', link: '/guide/basic-concepts' },
-          { text: 'Type of sources', link: '/guide/type-of-sources' },
-          { text: 'Consumption methods', link: '/guide/consumption-methods' },
-          { text: 'Modularization', link: '/guide/modularization' },
-          { text: 'Forking and merging', link: '/guide/forking-and-merging' },
-          { text: 'Error handling', link: '/guide/error-handling' },
-          { text: 'Embedded CSV parser/serializer', link: '/guide/embedded-csv-parser-serializer' },
-          { text: 'Operations on sorted data', link: '/guide/operations-on-sorted-data' },
-        ]
-      },      
-      {
-        text: 'Advanced topics',
-        collapsible: true,
-        items: [
-          { text: 'Exstream in depth', link: '/guide/exstream-in-depth' }
-        ]
-      },
-      {
-        text: 'Reference',
-        items: []
-      },
-      {
-        text: 'Filtering',
-        collapsible: true,
-        items: [
-          { text: 'filter', link: '/guide/reference/filter' },
-          { text: 'reject', link: '/guide/reference/reject' },
-          { text: 'find', link: '/guide/reference/find' },
-          { text: 'findWhere', link: '/guide/reference/findWhere' },
-          { text: 'compact', link: '/guide/reference/compact' },
-          { text: 'uniq', link: '/guide/reference/uniq' },
-          { text: 'uniqBy', link: '/guide/reference/uniqBy' },
-          { text: 'where', link: '/guide/reference/where' },    
-          { text: 'drop', link: '/guide/reference/drop' },
-          { text: 'head', link: '/guide/reference/head' },
-          { text: 'last', link: '/guide/reference/last' },
-          { text: 'slice', link: '/guide/reference/slice' },
-          { text: 'take', link: '/guide/reference/take' },
-        ]
-      },
-      {
-        text: 'Transforming',
-        collapsible: true,
-        items: [
-          { text: 'map', link: '/guide/reference/map' },
-          { text: 'omit', link: '/guide/reference/omit' },
-          { text: 'pick', link: '/guide/reference/pick' },
-          { text: 'pluck', link: '/guide/reference/pluck' },
-          { text: 'reduce', link: '/guide/reference/reduce' },
-          { text: 'reduce1', link: '/guide/reference/reduce1' },
-          { text: 'flatMap', link: '/guide/reference/flatMap' },
-          { text: 'flatten', link: '/guide/reference/flatten' },
-        ]
-      },
-      {
-        text: 'Grouping',
-        collapsible: true,
-        items: [
-          { text: 'collect', link: '/guide/reference/collect' },
-          { text: 'batch', link: '/guide/reference/batch' },
-          { text: 'keyBy', link: '/guide/reference/keyBy' },
-          { text: 'groupBy', link: '/guide/reference/groupBy' },    
-        ]
-      },
-      {
-        text: 'Working with buffers',
-        collapsible: true,
-        items: [
-          { text: 'split', link: '/guide/reference/split' },
-          { text: 'splitBy', link: '/guide/reference/splitBy' },
-          { text: 'csv', link: '/guide/reference/csv' },
-          { text: 'csvStringify', link: '/guide/reference/csvStringify' },
-          { text: 'decode', link: '/guide/reference/decode' },
-          { text: 'encode', link: '/guide/reference/encode' },
-        ]
-      },
-      {
-        text: 'Flow control',
-        collapsible: true,
-        items: [
-          { text: 'ratelimit', link: '/guide/reference/ratelimit' },
-          { text: 'throttle', link: '/guide/reference/throttle' },
-        ]
-      },
-      {
-        text: 'Handling Promises',
-        collapsible: true,
-        items: [
-          { text: 'resolve', link: '/guide/reference/resolve' },
-          { text: 'massThen', link: '/guide/reference/massThen' },
-          { text: 'massCatch', link: '/guide/reference/massCatch' },
-          { text: 'makeAsync', link: '/guide/reference/makeAsync' },
-          { text: 'asyncFilter', link: '/guide/reference/asyncFilter' },
-          { text: 'asyncReduce', link: '/guide/reference/asyncReduce' },    
-        ]
-      },
-      {
-        text: 'Handling errors',
-        collapsible: true,
-        items: [
-          { text: 'errors', link: '/guide/reference/errors' },
-          { text: 'stopOnError', link: '/guide/reference/stopOnError' },
-          { text: 'stopWhen', link: '/guide/reference/stopWhen' },
-        ]
-      },
-      {
-        text: 'Sorting',
-        collapsible: true,
-        items: [
-          { text: 'sort', link: '/guide/reference/sort' },
-          { text: 'sortBy', link: '/guide/reference/sortBy' },
-        ]
-      },
-      {
-        text: 'Transforming sorted data sources',
-        collapsible: true,
-        items: [
-          { text: 'sortedGroupBy', link: '/guide/reference/sortedGroupBy' },
-          { text: 'sortedJoin', link: '/guide/reference/sortedJoin' },
-        ]    
-      },
-      {
-        text: 'Side effects',
-        collapsible: true,
-        items: [
-          { text: 'tap', link: '/guide/reference/tap' },            
-        ]
-      },
-      {
-        text: 'Combining streams',
-        collapsible: true,
-        items: [
-          { text: 'merge', link: '/guide/reference/merge' },
-          { text: 'fork', link: '/guide/reference/fork' },
-          { text: 'observe', link: '/guide/reference/observe' },
-          { text: 'through', link: '/guide/reference/through' },
-          { text: 'pipeline', link: '/guide/reference/pipeline' },
-        ]
-      },
-      {
-        text: 'Consuming streams',
-        collapsible: true,
-        items: [
-          { text: 'toArray', link: '/guide/reference/toArray' },
-          { text: 'toPromise', link: '/guide/reference/toPromise' },
-          { text: 'pull', link: '/guide/reference/pull' },
-          { text: 'pipe', link: '/guide/reference/pipe' },
-          { text: 'each', link: '/guide/reference/each' },
-          { text: 'value', link: '/guide/reference/value' },
-          { text: 'values', link: '/guide/reference/values' },
-          { text: 'start', link: '/guide/reference/start' },
-          { text: 'consume', link: '/guide/reference/consume' },
-          { text: 'consumeSync', link: '/guide/reference/consumeSync' },
-        ]
-      },
-      {
-        text: 'Node.js streams interop',
-        collapsible: true,
-        items: [
-          { text: 'toAsyncIterator', link: '/guide/reference/toAsyncIterator' },
-          { text: 'toNodeStream', link: '/guide/reference/toNodeStream' },
-        ]
-      },
-      {
-        text: 'Controlling data flow',
-        collapsible: true,
-        items: [
-          { text: 'resume', link: '/guide/reference/resume' },
-          { text: 'pause', link: '/guide/reference/pause' },
-          { text: 'end', link: '/guide/reference/end' },
-          { text: 'destroy', link: '/guide/reference/destroy' },
-        ]
-      }
-    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Introduction',
+          collapsible: true,
+          items: [
+            { text: 'What is exstream.js?', link: '/guide/what-is-exstream' },
+            { text: 'Getting Started', link: '/guide/getting-started' }
+          ]
+        },
+        {
+          text: 'Guide',
+          collapsible: true,
+          items: [
+            { text: 'Basic concepts', link: '/guide/basic-concepts' },
+            { text: 'Type of sources', link: '/guide/type-of-sources' },
+            { text: 'Consumption methods', link: '/guide/consumption-methods' },
+            { text: 'Modularization', link: '/guide/modularization' },
+            { text: 'Forking and merging', link: '/guide/forking-and-merging' },
+            { text: 'Error handling', link: '/guide/error-handling' },
+            { text: 'Embedded CSV parser/serializer', link: '/guide/embedded-csv-parser-serializer' },
+            { text: 'Operations on sorted data', link: '/guide/operations-on-sorted-data' },
+          ]
+        },      
+        {
+          text: 'Advanced topics',
+          collapsible: true,
+          items: [
+            { text: 'Exstream in depth', link: '/guide/exstream-in-depth' }
+          ]
+        },
+      ],
+      '/reference/': [
+        {
+          text: 'Filtering',
+          collapsible: true,
+          items: [
+            { text: 'filter', link: '/reference/filter' },
+            { text: 'reject', link: '/reference/reject' },
+            { text: 'find', link: '/reference/find' },
+            { text: 'findWhere', link: '/reference/findWhere' },
+            { text: 'compact', link: '/reference/compact' },
+            { text: 'uniq', link: '/reference/uniq' },
+            { text: 'uniqBy', link: '/reference/uniqBy' },
+            { text: 'where', link: '/reference/where' },    
+            { text: 'drop', link: '/reference/drop' },
+            { text: 'head', link: '/reference/head' },
+            { text: 'last', link: '/reference/last' },
+            { text: 'slice', link: '/reference/slice' },
+            { text: 'take', link: '/reference/take' },
+          ]
+        },
+        {
+          text: 'Transforming',
+          collapsible: true,
+          items: [
+            { text: 'map', link: '/reference/map' },
+            { text: 'omit', link: '/reference/omit' },
+            { text: 'pick', link: '/reference/pick' },
+            { text: 'pluck', link: '/reference/pluck' },
+            { text: 'reduce', link: '/reference/reduce' },
+            { text: 'reduce1', link: '/reference/reduce1' },
+            { text: 'flatMap', link: '/reference/flatMap' },
+            { text: 'flatten', link: '/reference/flatten' },
+          ]
+        },
+        {
+          text: 'Grouping',
+          collapsible: true,
+          items: [
+            { text: 'collect', link: '/reference/collect' },
+            { text: 'batch', link: '/reference/batch' },
+            { text: 'keyBy', link: '/reference/keyBy' },
+            { text: 'groupBy', link: '/reference/groupBy' },    
+          ]
+        },
+        {
+          text: 'Working with buffers',
+          collapsible: true,
+          items: [
+            { text: 'split', link: '/reference/split' },
+            { text: 'splitBy', link: '/reference/splitBy' },
+            { text: 'csv', link: '/reference/csv' },
+            { text: 'csvStringify', link: '/reference/csvStringify' },
+            { text: 'decode', link: '/reference/decode' },
+            { text: 'encode', link: '/reference/encode' },
+          ]
+        },
+        {
+          text: 'Flow control',
+          collapsible: true,
+          items: [
+            { text: 'ratelimit', link: '/reference/ratelimit' },
+            { text: 'throttle', link: '/reference/throttle' },
+          ]
+        },
+        {
+          text: 'Handling Promises',
+          collapsible: true,
+          items: [
+            { text: 'resolve', link: '/reference/resolve' },
+            { text: 'massThen', link: '/reference/massThen' },
+            { text: 'massCatch', link: '/reference/massCatch' },
+            { text: 'makeAsync', link: '/reference/makeAsync' },
+            { text: 'asyncFilter', link: '/reference/asyncFilter' },
+            { text: 'asyncReduce', link: '/reference/asyncReduce' },    
+          ]
+        },
+        {
+          text: 'Handling errors',
+          collapsible: true,
+          items: [
+            { text: 'errors', link: '/reference/errors' },
+            { text: 'stopOnError', link: '/reference/stopOnError' },
+            { text: 'stopWhen', link: '/reference/stopWhen' },
+          ]
+        },
+        {
+          text: 'Sorting',
+          collapsible: true,
+          items: [
+            { text: 'sort', link: '/reference/sort' },
+            { text: 'sortBy', link: '/reference/sortBy' },
+          ]
+        },
+        {
+          text: 'Transforming sorted data sources',
+          collapsible: true,
+          items: [
+            { text: 'sortedGroupBy', link: '/reference/sortedGroupBy' },
+            { text: 'sortedJoin', link: '/reference/sortedJoin' },
+          ]    
+        },
+        {
+          text: 'Side effects',
+          collapsible: true,
+          items: [
+            { text: 'tap', link: '/reference/tap' },            
+          ]
+        },
+        {
+          text: 'Combining streams',
+          collapsible: true,
+          items: [
+            { text: 'merge', link: '/reference/merge' },
+            { text: 'fork', link: '/reference/fork' },
+            { text: 'observe', link: '/reference/observe' },
+            { text: 'through', link: '/reference/through' },
+            { text: 'pipeline', link: '/reference/pipeline' },
+          ]
+        },
+        {
+          text: 'Consuming streams',
+          collapsible: true,
+          items: [
+            { text: 'toArray', link: '/reference/toArray' },
+            { text: 'toPromise', link: '/reference/toPromise' },
+            { text: 'pull', link: '/reference/pull' },
+            { text: 'pipe', link: '/reference/pipe' },
+            { text: 'each', link: '/reference/each' },
+            { text: 'value', link: '/reference/value' },
+            { text: 'values', link: '/reference/values' },
+            { text: 'start', link: '/reference/start' },
+            { text: 'consume', link: '/reference/consume' },
+            { text: 'consumeSync', link: '/reference/consumeSync' },
+          ]
+        },
+        {
+          text: 'Node.js streams interop',
+          collapsible: true,
+          items: [
+            { text: 'toAsyncIterator', link: '/reference/toAsyncIterator' },
+            { text: 'toNodeStream', link: '/reference/toNodeStream' },
+          ]
+        },
+        {
+          text: 'Controlling data flow',
+          collapsible: true,
+          items: [
+            { text: 'resume', link: '/reference/resume' },
+            { text: 'pause', link: '/reference/pause' },
+            { text: 'end', link: '/reference/end' },
+            { text: 'destroy', link: '/reference/destroy' },
+          ]
+        }
+      ]
+    },
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2019-present Michele Triaca'
--- docs/index.md
+++ docs/index.md
@@ -13,19 +13,19 @@ hero:
       text: Guide
       link: /guide/what-is-exstream
     - theme: alt
+      text: Reference
+      link: /reference/filter
+    - theme: alt
       text: View on GitHub
       link: https://github.com/micheletriaca/exstream
 
 features:
-  - icon: ⚡️
-    title: Vite, The DX that can't be beat
-    details: Lorem ipsum...
-  - icon: 🖖
-    title: Power of Vue meets Markdown
-    details: Lorem ipsum...
-  - icon: 🛠️
-    title: Simple and minimal, always
-    details: Lorem ipsum...
+  - title: Easy to use
+    details: Manage streams with a syntax that resemble a vanilla Array's functional chain. Creating complex data flows has never been so easy
+  - title: Performant
+    details: Build stream chains that are asynchronous only when needed
+  - title: Versatile
+    details: the API can be easily extended to enhance the library with your specific use-cases
 ---
 
 <style>
